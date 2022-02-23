72237f076013088ba1a5fd4acf795ee734119558
Sébastien <apbseb@hotmail.fr>
2022-02-23 15:01:08 +0100

    fix: console.log useless removed

--- src/utils/provider-check.ts
+++ src/utils/provider-check.ts
@@ -1,7 +1,6 @@
 import { ICFProviderType } from '../@types';
 
 export const providerCheck = (): ICFProviderType => {
-  console.log(process.env);
   if (process.env.LAMBDA_TASK_ROOT && process.env.AWS_EXECUTION_ENV)
     return 'AWS';
   if (process.env.X_GOOGLE_FUNCTION_NAME) return 'GOOGLE';
