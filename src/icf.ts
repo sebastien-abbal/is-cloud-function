3fcb5b1b4ab763a5b8042ebbbe809fa0fa3170b4
Sébastien <apbseb@hotmail.fr>
2022-03-13 10:45:26 +0100

    fix: minor typescript fix on default type value

--- src/icf.ts
+++ src/icf.ts
@@ -1,11 +1,16 @@
-import { ICF_ConfigType, ICF_IConfig, ICF_Result } from './@types';
+import {
+  ICF_ConfigType,
+  ICF_EnumConfigType,
+  ICF_IConfig,
+  ICF_Result,
+} from './@types';
 import { providerCheck } from './utils/provider-check';
 
-export function isCloudFunction<T extends ICF_ConfigType>(
+export function isCloudFunction<T extends ICF_ConfigType = 'BOOLEAN'>(
   config?: ICF_IConfig<T>,
 ): ICF_Result<T> {
   const isCloudFunction = providerCheck();
-  if (config?.type === 'PROVIDER') {
+  if (config?.type === ICF_EnumConfigType.PROVIDER) {
     return (isCloudFunction ? isCloudFunction : null) as ICF_Result<T>;
   } else {
     return Boolean(isCloudFunction) as ICF_Result<T>;
