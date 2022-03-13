8ac4d181944467e304ad3be8174ded959b0fd817
Sébastien <apbseb@hotmail.fr>
2022-03-13 10:50:47 +0100

    fix: minor on enum default type icf function

--- src/icf.ts
+++ src/icf.ts
@@ -6,9 +6,9 @@ import {
 } from './@types';
 import { providerCheck } from './utils/provider-check';
 
-export function isCloudFunction<T extends ICF_ConfigType = 'BOOLEAN'>(
-  config?: ICF_IConfig<T>,
-): ICF_Result<T> {
+export function isCloudFunction<
+  T extends ICF_ConfigType = ICF_EnumConfigType.BOOLEAN,
+>(config?: ICF_IConfig<T>): ICF_Result<T> {
   const isCloudFunction = providerCheck();
   if (config?.type === ICF_EnumConfigType.PROVIDER) {
     return (isCloudFunction ? isCloudFunction : null) as ICF_Result<T>;
